769412b766908e4a9333634674ecde173e23a7d6
Long Huynh <longhuynhpyn01@gmail.com>
2022-05-25 22:55:39 +0700

    fixed some errror

--- src/pages/MoviePageV2.js
+++ src/pages/MoviePageV2.js
@@ -79,7 +79,7 @@ const MoviePageV2 = () => {
 
         {/* loading kiểu skeleton */}
         {loading && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
             {new Array(itemsPerPage).fill(0).map(() => (
               <MovieCardSkeleton key={v4()}></MovieCardSkeleton>
             ))}
